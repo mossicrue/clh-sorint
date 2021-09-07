a73aabf58d3fb4ab35091ea9357700a53c8d3215
Simone Mossi <mossicrue@gmail.com>
2021-09-07 12:44:37 +0200

    start test for condition while

--- katacoda-install.sh
+++ katacoda-install.sh
@@ -3,6 +3,8 @@
 # The script can be launched with this command in katacoda
 # curl -s https://raw.githubusercontent.com/mossicrue/clh-sorint/master/katacoda-install.sh | bash
 
+echo -e "Command Line Heroes Katacoda install script.\nThe installation process may take some times, please wait."
+
 # variables
 MANIFEST_PATH="https://raw.githubusercontent.com/mossicrue/clh-sorint/master/kubernetes/clh-all.yml"
 GAME_ROUTE_LINK=""
@@ -26,6 +28,17 @@ fi
 # Apply the all in one manifest
 (${OC_COMMAND} apply -f ${MANIFEST_PATH} &>/dev/null)
 
+# Wait for the Pod to be in running state
+echo -e "Installing the game pod, please wait"
+POD_STATUS="$(${OC_COMMAND} get pod -n clh-game --no-headers=true -o custom-columns=READY:.status.phase 2>/dev/null)"
+while [[ ${POD_STATUS} != "Running" ]]
+do
+  sleep 5s
+  echo -e "Installing the game pod, please wait"
+  POD_STATUS="$(${OC_COMMAND} get pod -n clh-game --no-headers=true -o custom-columns=READY:.status.phase 2>/dev/null)"
+done
+echo -e "Game POD Installed"
+
 # Get all the route in clh-game namespace
 GAME_ROUTE_LINK=$(${OC_COMMAND} get route clh-game -n clh-game --output=custom-columns=HOST:.spec.host --no-headers=true 2>/dev/null)
 if [[ -z "${GAME_ROUTE_LINK}" ]]
@@ -39,10 +52,10 @@ cat << EOF
 * COMMAND LINE HEROES *
 ***********************
 
-SUCCESS! The game is now up and running and is available at the following link, use Chrome for the best experience.
+SUCCESS! The game is now up and running at the following link:
 
 ${GAME_ROUTE_LINK}
 
-NOTE: Keep this page open while playing the game
+NOTE: Use Chrome for the best experience and keep this page open while playing the game!
 
 EOF
